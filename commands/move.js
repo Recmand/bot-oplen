84446e45b11e8dcae20e9625862b9000786a8d35
Glitch (octagonal-satin-chip) <none>
2021-10-29 08:39:18 +0000

    🌶🎢 Checkpoint ./commands/move.js:25901154/774

--- commands/move.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  name: "move",
-  alias:["move","mv"],
-  description: "move some user",
-  run: async(client, message) => ({ctx, member : discord.Member, channel : discord.VoiceChannel})
